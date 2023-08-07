ced482123fb8c2f7c0c9c3e97bbae864d773f956
Augusto Chebel Machado <116294307+AugustoCM27@users.noreply.github.com>
2023-08-07 13:31:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ st.header("st.header() - Isso é um cabeçalho")
 st.subheader("st.subheader() - Isso é um sub-cabeçalho")
 st.text("st.text() - Aqui você pode escrever o que quiser, é um texto")
 st.write("st.write() - Outra forma de escrever, mas com algumas opções adicionais...")
-st.markdown("st.markdown() - ")
 st.latex("st.latex() - sin^2(x) + cos^2(x) = 1")
 st.code("st.code() - print('Hello World')")
+
+st.text("Algumas opções adicionais:")
 
