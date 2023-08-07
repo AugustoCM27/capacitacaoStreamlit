d33a9d36b673c8303244b591805c94fb5b81047c
Augusto Chebel Machado <116294307+AugustoCM27@users.noreply.github.com>
2023-08-07 13:11:36 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Display Text
+st.title("st.title() - Isso é um título")
+st.header("st.header() - Isso é um cabeçalho")
+st.subheader("st.subheader() - Isso é um sub-cabeçalho")
+st.text("st.text() - Aqui você pode escrever o que quiser, é um texto")
+st.write("st.write() - Outra forma de escrever, mas com algumas opções adicionais...")
+st.markdown("st.markdown() - )
+
