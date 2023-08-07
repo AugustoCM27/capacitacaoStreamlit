8aefa26d3230a046ead7116cdadea730f875e678
Augusto Chebel Machado <116294307+AugustoCM27@users.noreply.github.com>
2023-08-07 13:38:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ st.header("st.header() - Isso é um cabeçalho")
 st.subheader("st.subheader() - Isso é um sub-cabeçalho")
 st.text("st.text() - Aqui você pode escrever o que quiser, é um texto")
 st.write("st.write() - Outra forma de escrever, mas com algumas opções adicionais...")
+
+expander = st.expander("Qual a diferença entre st.text e st.write?")
+expander.write("st.text: Este método é principalmente usado para exibir texto simples. 
+  Ele exibe o texto em uma fonte monoespaçada (como Courier) e não permite formatação avançada 
+  ou inserção de elementos interativos.")
+
 st.latex("st.latex() - sin^2(x) + cos^2(x) = 1")
 st.code("st.code() - print('Hello World')")
 
