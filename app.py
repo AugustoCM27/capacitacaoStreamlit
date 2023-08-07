3d6d70b35dce9d160cf4083ca259da13390a26e5
Augusto Chebel Machado <116294307+AugustoCM27@users.noreply.github.com>
2023-08-07 13:26:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.text("st.text() - Aqui você pode escrever o que quiser, é um texto")
 st.write("st.write() - Outra forma de escrever, mas com algumas opções adicionais...")
 st.markdown("st.markdown() - ")
 st.latex("sin^2(x) + cos^2(x) = 1")
-st.code("for i in range(0, 10): f(i)")
+st.code("for i in range(0, 10): \nf(i)")
 
