71f034af24e1d4218bc257a573086ce5c8f33dca
Augusto Chebel Machado <116294307+AugustoCM27@users.noreply.github.com>
2023-08-07 13:15:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.header("st.header() - Isso é um cabeçalho")
 st.subheader("st.subheader() - Isso é um sub-cabeçalho")
 st.text("st.text() - Aqui você pode escrever o que quiser, é um texto")
 st.write("st.write() - Outra forma de escrever, mas com algumas opções adicionais...")
-st.markdown("st.markdown() - )
+st.markdown("st.markdown() - ")
 
