d65204adb80d8142278baedcd94662c7084328b9
Augusto Chebel Machado <116294307+AugustoCM27@users.noreply.github.com>
2023-08-12 19:48:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ with tab_layouts:
   expander.write("O comando st.columns pode ser útil para você organizar a posição dos elementos em sua página")
   expander.write("Você pode criar as colunas e adicionar o que quiser com um 'with', como no exemplo:")
   expander.code("col1, col2 = st.columns(2)\n   with col1:\n      st.video()\n    with col2:\n      st.write('Descrição do vídeo')")
-  col1, col2 = st.columns(2)
+  col1, col2 = expander.columns(2)
   with col1:
     expander.video("https://www.youtube.com/watch?v=uQGxv-5lwTQ&list=PL-xocjZqCGjlJLqt7P7qqY-yxncdvKsLr")
   with col2:
